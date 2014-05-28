b5297f110419917bee87d4f71ab9f086d621ffd1
rcpeng34 <rcpeng@gmail.com>
2014-05-28 05:40:19 -0700

    Uses angular to display a hardcoded array of messages

--- frontpage/app.js
+++ frontpage/app.js
@@ -1,15 +1,28 @@
 'use strict';
 
 var app = angular.module('transcribeApp', []);
+var messages = [ 
+  {
+    speaker: 'user3',
+    time: '5:05:05',
+    text: 'This will be the text of a message from user 3'
+  },
+  {
+    speaker: 'user1',
+    time: '6:06:06',
+    text: 'This is a reponse from user 1'  
+  },
+  {
+    speaker: 'user2',
+    time: '6:06:06',
+    text: 'This is a reponse from user 2'  
+  }
+];
 
 app.run(function($rootScope){
   $rootScope.name = 'helloworld';
 });
 
 app.controller('MessageController', function($scope){
-  $scope.message = {
-    speaker: 'user2',
-    time: '5:05:05',
-    text: 'This will be the text of a message'
-  };
+  $scope.interview = messages;
 });
