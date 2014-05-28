e1f55028bdafca6dd385b7fed7f23dcc6b722df1
Adam D Richman <ar@adamrichman.com>
2014-05-28 02:25:27 -0700

    broken, but clients connect to each other

---
+++ server/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express     = require("express");
+var port        = process.env.PORT || 3000;
+var app         = express();
+var bodyParser  = require('body-parser');
+
+app.use(bodyParser());
+app.use(function(req, res, next) {
+
+  var origin = req.headers.origin || "*";
+
+  res.setHeader("access-control-allow-origin", origin);
+  res.setHeader("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("access-control-allow-headers", "content-type accept");
+  res.setHeader("access-control-max-age", 10);
+
+  if (req.method === "OPTIONS") { res.send(204) };
+  next();
+});
+app.use(express.static(__dirname + '/client'));
+require("./routes.js")(app);
+
+app.listen(port);
+console.log('Server listening on port: ' + port);
