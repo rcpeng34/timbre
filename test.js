f154c808d20c90b4fbf17822a2b8fa196cc93ea7
rcpeng34 <rcpeng@gmail.com>
2014-05-28 00:36:45 -0700

    Removes function wrapping

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-var stream;
+  var stream;
 
-var websocket_host = 'wss://api.wit.ai/speech_ws';
+  var websocket_host = 'wss://api.wit.ai/speech_ws';
 
-window.connection = new WebSocket(websocket_host);
+  window.connection = new WebSocket(websocket_host);
 
-connection.onopen = function(event) {
-    connection.send(JSON.stringify(['auth', 'ATMHGCDP74KFTTR647Y3ZUZCDSBMPJB6']));
-};
+  connection.onopen = function(event) {
+      connection.send(JSON.stringify(['auth', 'ATMHGCDP74KFTTR647Y3ZUZCDSBMPJB6']));
+  };
 
-connection.onclose = function(event) {
-    console.log('socket closed');
-};
+  connection.onclose = function(event) {
+      console.log('socket closed');
+  };
 
-connection.onmessage = function(event) {
-    var data = JSON.parse(event.data);
-    //data is tuple
-    if (data[0] === 'result') {
-        console.log(data[1].msg_body);
-    }
-};
+  connection.onmessage = function(event) {
+      var data = JSON.parse(event.data);
+      //data is tuple
+      if (data[0] === 'result') {
+          console.log(data[1].msg_body);
+      }
+  };
