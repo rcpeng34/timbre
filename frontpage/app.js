c855809459074673ce6ca1067f9462794372f9f6
rcpeng34 <rcpeng@gmail.com>
2014-05-28 05:13:12 -0700

    Uses angular to display a hardcoded message

---
+++ frontpage/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('transcribeApp', []);
+
+app.run(function($rootScope){
+  $rootScope.name = 'helloworld';
+});
+
+app.controller('MessageController', function($scope){
+  $scope.message = {
+    speaker: 'user2',
+    time: '5:05:05',
+    text: 'This will be the text of a message'
+  };
+});
